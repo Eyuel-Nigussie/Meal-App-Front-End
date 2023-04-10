a5ee53bd2bf78df12fae05bcd919252517e4d9d6
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-10 23:02:07 +0300

    amref pic customized

--- src/routes/notfound.svelte
+++ src/routes/notfound.svelte
@@ -1,12 +1,12 @@
- <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
-        <div className="max-w-lg mx-auto text-center">
-            <div className="pb-6">
-                <img src="https://floatui.com/logo.svg" width={150} className="mx-auto" />
+ <div class="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+        <div class="max-w-lg mx-auto text-center">
+            <div class="pb-6">
+                <img src="https://www.linkpicture.com/q/Meal_Planner-removebg-preview_1.png" width={250} class="mx-auto" />
             </div>
-            <h3 className="text-gray-800 text-4xl font-semibold sm:text-5xl">
+            <h   class="text-gray-800 text-4xl font-semibold sm:text-5xl">
                 Page not found
-            </h3>
-            <p className="text-gray-600 mt-3">
+            </h>
+            <p class="text-gray-600 mt-3">
                 Sorry, the page you are looking for could not be found or has been removed.
             </p>
         </div>
