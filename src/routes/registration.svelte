4eeacc29b9fd4c7798801b80ae08dedca20df699
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-11 19:45:15 +0300

    register page route added

--- src/routes/registration.svelte
+++ src/routes/registration.svelte
@@ -1,4 +1,19 @@
+<script>
+   import axios from 'axios';
+   import {push} from 'svelte-spa-router  '
 
+   let name = '', email = '', password;
+
+   $: submit = async () => {
+    await axios.post('',{
+      name: name,
+      email: email,
+      password: password
+    }),
+
+    await push('/login');
+   }
+</script>
  
 
 <div class="flex min-h-full items-center justify-center px-4 py-6 sm:px-6 lg:px-8">
@@ -13,25 +28,26 @@
       </div>
       <form class="space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true">
-        <div class="-space-y-px rounded-md shadow-sm">
-          <div>
-                <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
+        <div class="rounded-md shadow-sm">
+          <div class="my-4">
+                <label for="name" class="sr-only">Name</label>
+                <input bind:value={name} id="name" name="name" type="text" autocomplete="name" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Name">
+                <h6>{name}</h6>
          </div>
-          <div>
+          <div class="my-4">
                 <label for="email-address" class="sr-only">Email address</label>
                 <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 my-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
           </div>
-          <div>
+          <div class="my-4">
                 <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
+                <input bind:value={email} id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
           </div>
           
         </div>
   
         <div class="flex items-center justify-between">
           <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+            <input bind:value={password} id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
             <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
           </div>
   
