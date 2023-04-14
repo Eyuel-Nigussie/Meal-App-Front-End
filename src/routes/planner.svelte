449457b7fb326147190454bd2c5fff2e8652c255
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-14 18:28:22 +0300

    cal and sche wrapped together

---
+++ src/routes/planner.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Calendar from  './calendar.svelte'
+    import Schedule from './scheduler.svelte'
+</script>
+
+<section>
+    <Calendar />
+    <Schedule />
+</section>
+
+<style>
+
+</style>
