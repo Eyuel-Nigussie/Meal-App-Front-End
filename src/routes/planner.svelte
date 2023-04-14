07dd2c74e09caf7d7c86c7a466a9c9593bc4c19d
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-14 18:58:36 +0300

    scheduler modal open and close

--- src/routes/planner.svelte
+++ src/routes/planner.svelte
@@ -1,11 +1,15 @@
 <script>
     import Calendar from  './calendar.svelte'
     import Schedule from './scheduler.svelte'
+
+    let schedulerShowing = false;
 </script>
 
 <section>
-    <Calendar />
-    <Schedule />
+    <Calendar on:click={() => schedulerShowing = true}/>
+    {#if schedulerShowing}
+       <Schedule on:click={() => schedulerShowing = false }/>
+    {/if}
 </section>
 
 <style>
