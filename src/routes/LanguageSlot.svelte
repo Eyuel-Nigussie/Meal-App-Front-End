9ec1768ceeadead2d5a12dd686062fd36a38c633
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-20 00:49:06 +0300

    multiple select working

---
+++ src/routes/LanguageSlot.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let option
+  export let height = `20px`
+  export let gap = '5pt'
+
+  const repo = `https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons`
+
+  $: lang = String(option)
+    .toLowerCase()
+    .replaceAll(`+`, `p`)
+    .replace(`#`, `sharp`)
+    .replace(`javascript`, `js`)
+    .replace(`dart`, `dartlang`)
+  $: src = `${repo}/file_type_${lang}.svg`
+
+  let hidden = false
+  // default back to visible every time src changes to see if the image loads successfully
+  $: src, (hidden = false)
+</script>
+
+<span style:gap>
+  <!-- <img {src} {height} alt={lang} {hidden} on:error={() => (hidden = true)} /> -->
+  {option}
+</span>
+
+<style>
+  span {
+    display: flex;
+  }
+</style>
