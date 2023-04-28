5b1ffc109d80d37d0ed2bbdd52fff8f59f2af517
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-28 17:02:38 +0300

    labels modified

--- src/routes/registration.svelte
+++ src/routes/registration.svelte
@@ -32,21 +32,16 @@
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm">
           <div class="my-4">
-                <label for="name" class="sr-only">Name</label>
+                <h2>Email</h2>
                 <input bind:value={name} id="name" name="name" type="text" autocomplete="name" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Name">
-                <h2>{name}</h2>
          </div>
           <div class="my-4">
-                <label for="email-address" class="sr-only">Email address</label>
+                <h2>Email</h2>
                 <input bind:value={email} id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 my-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Email address">
-                <h2>email</h2>
-                <h2>{email}</h2>
          </div>
           <div class="my-4">
-                <label for="password" class="sr-only">Password</label>
-                <input bind:value={password} id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Password">
                 <h2>Password</h2>
-                <h2>{password}</h2>
+                <input bind:value={password} id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Password">
           </div>
             {#if has_error}
                 <h1>Registration Failed</h1>
