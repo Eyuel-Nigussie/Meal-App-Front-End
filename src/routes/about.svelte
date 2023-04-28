6d64ef0c6751e9d74fff3e758fbf3bd12e567fa1
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-28 11:08:58 +0300

    customized

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,27 +1,76 @@
-<script>
-    import { AccordionItem, Accordion } from 'flowbite-svelte'
-  </script>
-  
-
-<Accordion>
-  <h1 class="text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">FAQ</h1>
-    <AccordionItem> 
-      <span slot="header" class="text-base flex gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"/></svg>
-        <span> My Header 1</span>
-      </span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo...</p>
-      <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start  websites even faster with components on top of Tailwind CSS.</p>
-    </AccordionItem>
-    <AccordionItem>
-      <span slot="header" class="text-base flex gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"/></svg>
-        <span> My Header 2</span>
-      </span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sintexplicabo...</p>
-    </AccordionItem>
-  </Accordion>  
-
+  <section class="bg-white">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-black">Frequently asked <span class="text-red-600">Questions</span></h2>
+        <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 md:grid-cols-2">
+            <div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        What do you mean by "Figma assets"?
+                    </h3>
+                    <p class="text-gray-500">You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.</p>
+                </div>
+                <div class="mb-10">                        
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        What does "lifetime access" exactly mean?
+                    </h3>
+                    <p class="text-gray-500">Once you have purchased either the design, code, or both packages, you will have access to all of the future updates based on the roadmap, free of charge.</p>
+                </div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        How does support work?
+                    </h3>
+                    <p class="text-gray-500">We're aware of the importance of well qualified support, that is why we decided that support will only be provided by the authors that actually worked on this project.</p>
+                    <p class="text-gray-500">Feel free to <a href="#" class="font-medium underline text-primary-600 hover:no-underline" target="_blank" rel="noreferrer">contact us</a> and we'll help you out as soon as we can.</p>
+                </div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        I want to build more than one project. Is that allowed?
+                    </h3>
+                    <p class="text-gray-500">You can use Windster for an unlimited amount of projects, whether it's a personal website, a SaaS app, or a website for a client. As long as you don't build a product that will directly compete with Windster either as a UI kit, theme, or template, it's fine.</p>
+                    <p class="text-gray-500">Find out more information by <a href="#" class="font-medium underline text-primary-600 hover:no-underline">reading the license</a>.</p>
+                </div>
+            </div>
+            <div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        What does "free updates" include?
+                    </h3>
+                    <p class="text-gray-500">The free updates that will be provided is based on the <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">roadmap</a> that we have laid out for this project. It is also possible that we will provide extra updates outside of the roadmap as well.</p>
+                </div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        What does the free version include?
+                    </h3>
+                    <p class="text-gray-500">The <a href="#" class="font-medium underline text-primary-600 hover:no-underline">free version</a> of Windster includes a minimal style guidelines, component variants, and a dashboard page with the mobile version alongside it.</p>
+                    <p class="text-gray-500">You can use this version for any purposes, because it is open-source under the MIT license.</p>
+                </div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        What is the difference between Windster and Tailwind UI?
+                    </h3>
+                    <p class="text-gray-500">Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.</p>
+                    <p class="text-gray-500">Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.</p>
+                </div>
+                <div class="mb-10">
+                    <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                        <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                        Can I use Windster in open-source projects?
+                    </h3>
+                    <p class="text-gray-500">Generally, it is accepted to use Windster in open-source projects, as long as it is not a UI library, a theme, a template, a page-builder that would be considered as an alternative to Windster itself.</p>
+                    <p class="text-gray-500">With that being said, feel free to use this design kit for your open-source projects.</p>
+                    <p class="text-gray-500">Find out more information by <a href="#" class="font-medium underline text-primary-600 hover:no-underline">reading the license</a>.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  </section>
 
 <!-- ====== Brands Section Start -->
 <section class="bg-white py-20 lg:py-[120px]">
@@ -34,9 +83,9 @@
               class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
             >
               <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/graygrids.svg"
+                src="https://igad.int/wp-content/uploads/2021/07/Green-Logo.svg"
                 alt="image"
-                class="h-10 w-full"
+                class="h-20 w-full"
               />
             </a>
             <a
@@ -44,9 +93,9 @@
               class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
             >
               <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/lineicons.svg"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/USAID-Identity.svg/768px-USAID-Identity.svg.png?20230407133557"
                 alt="image"
-                class="h-10 w-full"
+                class="h-20 w-full"
               />
             </a>
             <a
@@ -54,9 +103,9 @@
               class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
             >
               <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/uideck.svg"
+                src="https://1000logos.net/wp-content/uploads/2021/03/UNICEF-logo-768x432.png"
                 alt="image"
-                class="h-10 w-full"
+                class="h-20 w-full"
               />
             </a>
             <a
@@ -64,9 +113,9 @@
               class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
             >
               <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/brands/ayroui.svg"
+                src="https://www.linkpicture.com/q/Meal_Planner-removebg-preview_1.png"
                 alt="image"
-                class="h-10 w-full"
+                class="h-29 w-full"
               />
             </a>
           </div>
@@ -78,7 +127,7 @@
   
 
 <!-- ====== About Section Start -->
-<section class="overflow-hidden pt-20 pb-12 lg:pt-[120px] lg:pb-[90px]">
+<section class="overflow-hidden pt-20 pb-12 px-4 lg:pt-[120px] lg:pb-[90px]">
     <div class="container mx-auto">
       <div class="-mx-4 flex flex-wrap items-center justify-between">
         <div class="w-full px-4 lg:w-6/12">
@@ -86,26 +135,28 @@
             <div class="w-full px-3 sm:px-4 xl:w-1/2">
               <div class="py-3 sm:py-4">
                 <img
-                  src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-1.jpg"
+                  src="https://amrefcanada.org/wp-content/uploads/2021/04/amref-health-person.jpg"
                   alt=""
                   class="w-full rounded-2xl"
                 />
               </div>
               <div class="py-3 sm:py-4">
                 <img
-                  src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-2.jpg"
+                  src="https://www.airmedandrescue.com/sites/default/files/styles/full_size_view/public/2020-05/amref-flying-doctors-acquires-isolation-chambers-covid.jpg?h=ac3819c7&itok=t5XVjJTI"
                   alt=""
                   class="w-full rounded-2xl"
                 />
               </div>
+              
             </div>
             <div class="w-full px-3 sm:px-4 xl:w-1/2">
               <div class="relative z-10 my-4">
                 <img
-                  src="https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-3.jpg"
+                  src="https://i0.wp.com/amref.org/wp-content/uploads/2022/08/Amref-Ethiopia-at-a-Glance-2021.jpg?w=1089&ssl=1"
                   alt=""
                   class="w-full rounded-2xl"
                 />
+                
                 <span class="absolute -right-7 -bottom-7 z-[-1]">
                   <svg
                     width="134"
@@ -683,10 +734,10 @@
         <div class="w-full px-4 lg:w-1/2 xl:w-5/12">
           <div class="mt-10 lg:mt-0">
             <span class="text-primary mb-2 block text-lg font-semibold">
-              Why Choose Us
+              Why Choose <span class="text-red-500">Us</span>
             </span>
-            <h2 class="text-dark mb-8 text-3xl font-bold sm:text-4xl">
-              Make your customers happy by giving services.
+            <h2 class="text- mb-8 text-3xl font-bold sm:text-4xl">
+              Make your customers <span class="text-red-600 text-red-500">happy</span> by giving services.
             </h2>
             <p class="text-body-color mb-8 text-base">
               It is a long established fact that a reader will be distracted by
@@ -713,7 +764,7 @@
   
 
 
-
+<!-- 
   <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
@@ -786,6 +837,6 @@
           </div>
       </div>
     </div>
-</footer>
+</footer> -->
 
   
