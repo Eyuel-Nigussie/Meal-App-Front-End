7bc41976018ac4ee7d5aa31d9a7b8aaf95611b55
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-05-01 19:08:29 +0300

    customized

--- src/routes/restrictions.svelte
+++ src/routes/restrictions.svelte
@@ -1,3 +1,4 @@
+<!-- ============================================================================================= -->
 <script>
   let selectedPreferences = [];
 
@@ -15,11 +16,11 @@
   <!-- Features Section -->
   <section id="features">
       <!-- Flex Container -->
-      <div class="container flex flex-col px-4 mx-auto mt-10 mb-10 space-y-12 md:space-y-0 md:flex-row">
+      <div class="container flex flex-col px-4 mx-auto md:space-y-0 md:flex-row">
               <!-- What's Different -->
-          <div class="flex flex-col space-y-12  md:w-1/2 ">
-              <img class="object-cover w-full rounded-t-lg w-76 mx-auto md:w-96  md:rounded-none md:rounded-l-lg" src="https://graphicriver.img.customer.envatousercontent.com/files/325941439/preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=67f091d15d672a6bc8f952e30c830d8a" alt="">  
-          </div>
+            <div class="flex flex-col space-y-12  md:w-1/2 ">
+                <img class="object-cover w-full rounded-t-lg w-76 mx-auto md:w-96  md:rounded-none md:rounded-l-lg" src="https://graphicriver.img.customer.envatousercontent.com/files/325941439/preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=67f091d15d672a6bc8f952e30c830d8a" alt="">  
+            </div>
 
                           <!-- Numbered List -->
           <div class="flex flex-col space-y-8 md:w-1/2">
@@ -30,22 +31,24 @@
                       <button class:selected={selectedPreferences.includes('gluten-free')} on:click={() => togglePreference('gluten-free')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Gluten-free</button>
                       <button class:selected={selectedPreferences.includes('low-carb')} on:click={() => togglePreference('low-carb')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Low-carb</button>
                       <button class:selected={selectedPreferences.includes('vegan')} on:click={() => togglePreference('vegan')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Vegan</button>
-                      <button class:selected={selectedPreferences.includes('vegetarian')} on:click={() => togglePreference('vegetarian')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Vegetarian</button>
-                      <button class:selected={selectedPreferences.includes('gluten-free')} on:click={() => togglePreference('gluten-free')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Gluten-free</button>
-                      <button class:selected={selectedPreferences.includes('low-carb')} on:click={() => togglePreference('low-carb')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Low-carb</button>
-                      <button class:selected={selectedPreferences.includes('vegetarian')} on:click={() => togglePreference('vegetarian')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Vegetarian</button>
-                      <button class:selected={selectedPreferences.includes('gluten-free')} on:click={() => togglePreference('gluten-free')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Gluten-free</button>
-                      <button class:selected={selectedPreferences.includes('low-carb')} on:click={() => togglePreference('low-carb')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Low-carb</button>
+                      <button class:selected={selectedPreferences.includes('Pork')} on:click={() => togglePreference('Pork')} class="m-2 bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-full border border-gray-300 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 active:bg-gray-400">Pork</button>
                     </div>
               </div>
           </div>
+          <a href="/#/login">
+            <button type="button" class="md:hidden text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">CONTINUE</button>
+          </a>
       </div> 
-  </section> s
+      <a href="/#/login">
+        <button type="button" class="hidden md:block mx-auto mt-[-100px] text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">CONTINUE</button>
+      </a>
+
+  </section> 
 
 
 
 <style>
-    .selected {
+  .selected {
     background-color: #d46161; /* Change this to the desired color */
     color: white;
   }
